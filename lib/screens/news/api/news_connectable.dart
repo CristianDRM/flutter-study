cc68662e32a377f87acc30f839d957a229313720
Cristian Madrid <cristianmadridd@gmail.com>
2019-01-13 10:50:49 -0200

    adding tests for news connector

--- lib/screens/news/api/news_connectable.dart
+++ lib/screens/news/api/news_connectable.dart
@@ -2,17 +2,17 @@ import 'dart:convert';
 import 'package:http/http.dart' show Client;
 import 'package:exercise/screens/news/api/news.dart';
 
-abstract class NewsConnectable {
-  Future<List<News>> fetchNews();
-}
+class NewsConnector {
+  Client client = Client();
+  final _api = 'https://hacker-news.firebaseio.com/v0';
 
-class NewsConnector extends NewsConnectable {
+  fetchTopIds() async {
+    final response = await client.get('$_api/topstories.json');
+    return jsonDecode(response.body);
+  }
 
-  @override
-  Future<List<News>> fetchNews() async {
-    final client = Client();
-    final response = await client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-    final json = jsonDecode(response.body) as List;
-    return json.map((item) => News.fromJson(item));
+  fetchItem({int id}) async {
+    final response = await client.get('$_api/item/$id.json');
+    return News.fromJson(jsonDecode(response.body));
   }
 }
--- lib/screens/news/list/news_list.dart
+++ lib/screens/news/list/news_list.dart
@@ -1,3 +1,5 @@
+import 'package:exercise/screens/news/api/news.dart';
+import 'package:exercise/screens/news/news_bloc.dart';
 import 'package:flutter/material.dart';
 
 class NewsList extends StatefulWidget {
@@ -6,11 +8,43 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
+
+  final NewsBloc bloc = NewsBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchNews();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("News")),
-      body: Container(child: Text("hello"))
+      body: StreamBuilder(
+          stream: bloc.news,
+          builder: (BuildContext context, AsyncSnapshot<List<News>> snapshot) {
+            switch(snapshot.connectionState) {
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return CircularProgressIndicator();
+              default:
+                if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                }
+            }
+            ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return newsItem(news: snapshot.data[index]);
+                }
+            );
+          }
+      )
     );
+  }
+
+  Widget newsItem({News news}) {
+    return Text(news.title);
   }
 }
